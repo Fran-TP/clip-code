6df43025a3fdaa0ff277de4eec9c295b07dd7f61
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-24 08:53:00 -0500

    Create Home page layout with navigation links

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page!</h1>
+      <p className="text-lg">This is a simple example of a home page.</p>
+    </div>
+  )
+}
+
+export default Home
