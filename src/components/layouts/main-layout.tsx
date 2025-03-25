e588164ca698807bb657d84dced07b59eaaae4f9
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-25 08:45:00 -0500

    Refactor MainLayout component to use Header, Sidebar, and Progressbar for improved layout structure

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,38 +1,19 @@
-import { Outlet, NavLink } from 'react-router'
+import { Outlet } from 'react-router'
+import Header from '@components/molecules/header'
+import Sidebar from '@components/molecules/sidenav'
+import Progressbar from '@components/atoms/progressbar'
 
 const MainLayout: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen h-screen">
-      <header className="bg-gray-800 text-white p-4">
-        <h1 className="text-2xl">My Application</h1>
-      </header>
-      <nav>
-        <ul className="flex space-x-4 p-4 bg-gray-700 text-white">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? 'text-yellow-500' : '')}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/create"
-              className={({ isActive }) => (isActive ? 'text-yellow-500' : '')}
-            >
-              Create Snippet
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <main className="flex-1 p-4">
-        {/* This is where the child routes will be rendered */}
-        <Outlet />
-      </main>
-      <footer className="bg-gray-800 text-white p-4">
-        <p>&copy; 2023 My Application</p>
-      </footer>
+    <div className="flex flex-col min-h-screen h-screen text-gray-200 bg-gray-950">
+      <Header />
+      <Progressbar />
+      <div className="flex flex-1 pt-16">
+        <Sidebar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
