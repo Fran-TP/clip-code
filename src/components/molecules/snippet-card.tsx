af15f574675fb3b73c58fe7aa85cec70641f7629
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-26 09:23:00 -0500

    feat: add SnippetCard component for displaying code snippets with copy functionality

---
+++ src/components/molecules/snippet-card.tsx
@@ -0,0 +1,76 @@
+import { Check, Copy } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
+import { useEffect, useState } from 'react'
+import { codeToHtml } from 'shiki'
+
+interface SnippetCardProps {
+  title: string
+  rawValue: string
+}
+
+const SnippetCard: React.FC<SnippetCardProps> = ({ title, rawValue }) => {
+  const [code, setCode] = useState<string | null>(null)
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleClickCopy = () => {
+    navigator.clipboard.writeText(rawValue)
+
+    setIsCopied(true)
+    setTimeout(() => setIsCopied(false), 600)
+  }
+
+  useEffect(() => {
+    const convertCodeToHtml = async () => {
+      const html = await codeToHtml(rawValue, {
+        theme: 'github-dark-default',
+        lang: 'javascript'
+      })
+      setCode(html)
+    }
+    convertCodeToHtml()
+  }, [rawValue])
+
+  if (!code) {
+    return (
+      <div className="flex items-center justify-center h-full p-4">
+        <p className="text-gray-500 text-sm">Loading...</p>
+      </div>
+    )
+  }
+
+  return (
+    <article className="border-2 border-gray-800 rounded-lg overflow-clip">
+      <header className="p-2 flex items-center justify-between">
+        <h2 className="text-sm font-medium">{title}</h2>
+        <button
+          type="button"
+          className="group p-2 cursor-pointer border-2 border-gray-800 rounded-sm hover:bg-gray-900 transition-colors duration-200 inline-flex items-center justify-center"
+          onClick={handleClickCopy}
+        >
+          <AnimatePresence mode="wait" initial={false}>
+            <motion.span
+              key={isCopied ? 'copied' : 'copy'}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={{ duration: 0.2 }}
+              className="inline-flex items-center justify-center"
+            >
+              {isCopied ? (
+                <Check className="opacity-70 group-hover:opacity-100 size-4" />
+              ) : (
+                <Copy className="opacity-70 group-hover:opacity-100 size-4" />
+              )}
+            </motion.span>
+          </AnimatePresence>
+        </button>
+      </header>
+      <div
+        className="overflow-clip"
+        dangerouslySetInnerHTML={{ __html: code }}
+      />
+    </article>
+  )
+}
+
+export default SnippetCard
