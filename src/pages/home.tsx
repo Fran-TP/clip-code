b38d080dac614747d6802a843d188b9402a1fb25
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-26 08:55:00 -0500

    feat: implement Masonry layout for code snippets on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,136 @@
+import MasonryLayout from '@components/atoms/masonry'
+import SnippetCard from '@components/molecules/snippet-card'
+
+const snippets = [
+  {
+    id: 1,
+    title: 'Sample Snippet',
+    description: 'This is a sample snippet description.',
+    code: `const hello = 'Hello, world!';\nconsole.log(hello);`
+  },
+  {
+    id: 2,
+    title: 'Another Snippet',
+    description: 'This is another snippet description.',
+    code: `const greet = name => \`Hello, \${name}!\`;\ngreet('Alice');`
+  },
+  {
+    id: 3,
+    title: 'Yet Another Snippet',
+    description: 'This is yet another snippet description.',
+    code: 'const add = (a, b) => a + b;\nconsole.log(add(2, 3));'
+  },
+  {
+    id: 4,
+    title: 'Button ant design',
+    description: 'This is a sample snippet description.',
+    code: `import React from 'react';
+import { Button, Flex } from 'antd';
+
+const App: React.FC = () => (
+  <Flex gap="small" wrap>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </Flex>
+);
+
+export default App;`
+  },
+  {
+    id: 5,
+    title: 'Button material ui',
+    description: 'This is a sample snippet description.',
+    code: `import React from 'react';
+import { Button, Stack } from '@mui/material';
+
+const MaterialButton: React.FC = () => (
+  <Stack spacing={2}>
+    <Button variant="contained">Contained Button</Button>
+    <Button variant="outlined">Outlined Button</Button>
+    <Button variant="text">Text Button</Button>
+  </Stack>
+);
+
+export default MaterialButton;`
+  },
+  {
+    id: 6,
+    title: 'Buttons, Flex ant design',
+    description: 'This is a sample snippet description.',
+    code: `import { Masonry } from 'react-plock'
+
+interface MasonryProps {
+  items: { id: number; title: string; description: string; code: string }[]
+  children: (item: {
+    id: number
+    title: string
+    description: string
+    code: string
+  }) => React.ReactNode
+}
+
+const CONFIG = {
+  columns: [1, 2],
+  gap: [16, 16],
+  media: [640, 768]
+}
+
+const MasonryLayout: React.FC<MasonryProps> = ({ items, children }) => {
+  return (
+    <Masonry items={items} config={CONFIG} render={item => children(item)} />
+  )
+}
+
+export default MasonryLayout
+`
+  },
+  {
+    id: 7,
+    title: 'Shiki and monaco',
+    description: 'This is a sample snippet description.',
+    code: `import React, { useEffect, useRef } from 'react';
+import * as monaco from 'monaco-editor';
+
+function MonacoEditor() {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      monaco.editor.create(editorRef.current, {
+        value: '// Code here',
+        language: 'javascript',
+        lineDecorationsWidth: 20, // Example: increased padding
+        theme: 'vs-dark',
+        // ... other options
+      });
+    }
+  }, []);
+
+  return (
+    <div
+      style={{ width: '100%', height: '500px' }}
+      ref={editorRef}
+    ></div>
+  );
+}
+
+export default MonacoEditor;`
+  }
+]
+
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page!</h1>
-      <p className="text-lg">This is a simple example of a home page.</p>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold mb-4">Snippets</h1>
+      <MasonryLayout items={snippets}>
+        {item => (
+          <SnippetCard key={item.id} title={item.title} rawValue={item.code} />
+        )}
+      </MasonryLayout>
+    </>
   )
 }
 
