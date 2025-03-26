c7d6cb389f46d0677958af5cfaf7004754e5e7f6
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-26 09:17:00 -0500

    refactor: simplify header component by removing unnecessary imports and links

--- src/components/molecules/header.tsx
+++ src/components/molecules/header.tsx
@@ -1,13 +1,9 @@
-import { Link } from 'react-router'
-
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 h-16 border-b-2 border-gray-800 px-4">
+    <header className="shrink-0 h-16 border-b-2 border-gray-800 px-4">
       <div className="h-full flex items-center justify-between">
         <div className="flex items-center gap-2 md:gap-6">
-          <Link to="/" className="">
-            Home
-          </Link>
+          <h1 className="text-xl font-bold">ClipCode</h1>
         </div>
       </div>
     </header>
