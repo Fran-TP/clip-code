5a301425d700d1f50c10766bf4aa6119ed6cdc31
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-27 08:28:00 -0500

    Add page transition animations to MainLayout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,18 +1,28 @@
-import { Outlet } from 'react-router'
+import { Outlet, useLocation } from 'react-router'
 import Header from '@components/molecules/header'
 import Sidebar from '@components/molecules/sidenav'
 import Progressbar from '@components/atoms/progressbar'
+import { AnimatePresence, motion } from 'motion/react'
 
 const MainLayout: React.FC = () => {
+  const { pathname } = useLocation()
+
   return (
     <div className="flex flex-col min-h-screen h-screen text-gray-200 bg-gray-950">
       <Header />
       <Progressbar />
-      <div className="flex flex-1 pt-16">
+      <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1">
-          <Outlet />
-        </main>
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.main
+            key={pathname}
+            initial={{ y: 20 }}
+            animate={{ y: 0 }}
+            className="flex flex-col flex-1 p-4 overflow-y-auto"
+          >
+            <Outlet />
+          </motion.main>
+        </AnimatePresence>
       </div>
     </div>
   )
