128c85ba7d6b79d04e282e4c8c8cf1c3b0f32d7b
Fran Torres <ftorresp@unasam.edu.pe>
2025-03-27 08:43:00 -0500

    Refactor create-snippet page to use FormSnippet component

--- src/pages/create-snippet.tsx
+++ src/pages/create-snippet.tsx
@@ -1,6 +1,5 @@
-import Label from '@components/atoms/label'
-import Input from '@components/atoms/input'
 import EditorCode from '@components/molecules/editor-code'
+import FormSnippet from '@components/molecules/form-snippet'
 
 const CreateSnippet: React.FC = () => {
   return (
@@ -9,33 +8,7 @@ const CreateSnippet: React.FC = () => {
       <div className="grid flex-1 grid-cols-1 md:grid-cols-2 gap-4">
         <div className="border-2 border-gray-800 rounded-md p-4 flex flex-col gap-4 bg-base">
           <h2 className="text-xl font-semibold">Snippet Details</h2>
-          <form className="space-y-4">
-            <Label htmlFor="title" className="flex flex-col gap-3">
-              Title
-              <Input
-                id="title"
-                type="text"
-                name="title"
-                placeholder="Title of the snippet"
-                autoComplete="off"
-              />
-            </Label>
-            <Label htmlFor="description" className="flex flex-col gap-3">
-              Description
-              <textarea
-                id="description"
-                name="description"
-                placeholder="Description of the snippet"
-                className="border-2 border-gray-800 field-sizing-content resize-none px-3 bg-gray-950 py-2 rounded-sm outline-2 outline-transparent focus-visible:outline-cyan-500 transition-colors duration-200"
-              />
-            </Label>
-            <button
-              type="submit"
-              className="border-2 border-cyan-500 rounded-md bg-base cursor-pointer"
-            >
-              Create Snippet
-            </button>
-          </form>
+          <FormSnippet />
         </div>
         <div className="border-2 border-gray-800 overflow-clip rounded-md flex flex-col">
           <div className="p-2">
