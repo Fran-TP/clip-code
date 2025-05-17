0a41db92aae9e931d1b8eb6f8d8929255c95fb35
Fran Torres <ftorresp@unasam.edu.pe>
2025-05-16 20:10:14 -0500

    Add fetchCategoriesWithLanguages service function

---
+++ src/services/fetchCategoriesWithLanguages.ts
@@ -0,0 +1,24 @@
+import { db } from '@lib/constants/dbConfig'
+import type {
+  CategoriesWithLanguages,
+  CategoriesWithLanguagesMap
+} from '@lib/types'
+
+const fetchCategoriesWithLanguages =
+  async (): Promise<CategoriesWithLanguagesMap> => {
+    const result = await db.select<CategoriesWithLanguages[]>(`
+    SELECT c.category_id categoryId, c.name category,l.language_id languageId, l.name language
+      FROM languages_categories lc
+        JOIN main.categories c on c.category_id = lc.fk_category_id
+        JOIN main.languages l on l.language_id = lc.fk_language_id;
+  `)
+
+    const groupedByCategories = Object.groupBy(
+      result,
+      ({ category }) => category
+    )
+
+    return groupedByCategories
+  }
+
+export default fetchCategoriesWithLanguages
