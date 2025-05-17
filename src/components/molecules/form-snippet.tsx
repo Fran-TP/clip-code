17f131b8694a5fafeb716b77c469e53fb016d254
Fran Torres <ftorresp@unasam.edu.pe>
2025-05-16 20:09:15 -0500

    Refactor language selection to use LanguagePicker component

--- src/components/molecules/form-snippet.tsx
+++ src/components/molecules/form-snippet.tsx
@@ -1,29 +1,24 @@
 import Button from '@components/atoms/button'
 import Input from '@components/atoms/input'
 import Label from '@components/atoms/label'
-import clsx from 'clsx'
+import LanguagePicker from '@components/molecules/language-picker'
+import { extractLanguages } from '@lib/helpers/normalizeCategories'
+import type { LoaderDataCreateSnippet } from '@lib/types'
 import Fuse from 'fuse.js'
-import { Check, Search } from 'lucide-react'
+import { Search } from 'lucide-react'
 import { useState } from 'react'
 import { useLoaderData } from 'react-router'
 
 const FormSnippet = () => {
-  const { categoriesWithLanguages, languages } = useLoaderData<{
-    categoriesWithLanguages: Record<
-      string,
-      { categoryName: string; langName: string }[]
-    >
-    languages: { langId: string; langName: string }[]
-  }>()
+  const { categoriesWithLanguages, languages } =
+    useLoaderData<LoaderDataCreateSnippet>()
 
   const [selectedCategory, setSelectedCategory] = useState('General')
-  const [selectedLang, setSelectedLang] = useState<string | null>(null)
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null)
   const [searchTerm, setSearchTerm] = useState('')
 
-  console.log(languages)
-
   const fuse = new Fuse(languages, {
-    keys: ['langName']
+    keys: ['language']
   })
   const filteredResult = fuse.search(searchTerm, {
     limit: 20
@@ -41,6 +36,15 @@ const FormSnippet = () => {
     const searchValue = event.currentTarget.value
 
     setSearchTerm(searchValue)
+  }
+
+  const handleSelectedLanguage = (language: string) => () => {
+    setSelectedLanguage(language)
+  }
+
+  const handleSelectedCategory = (category: string) => () => {
+    setSearchTerm('')
+    setSelectedCategory(category)
   }
 
   return (
@@ -68,6 +72,7 @@ const FormSnippet = () => {
         <div className="p-3 relative">
           <Input
             type="search"
+            name="search"
             value={searchTerm}
             placeholder="Search language"
             onChange={handleSearchChange}
@@ -75,96 +80,18 @@ const FormSnippet = () => {
           />
           <Search className="absolute left-5 transform -translate-y-1/2 size-5 top-1/2 text-gray-200 opacity-70 peer-focus-visible:opacity-100 peer-focus-visible:text-cyan-500 transition-all" />
         </div>
-        <div className="flex overflow-clip h-72">
-          <div className="w-1/3 h-full border-r-2 border-gray-800 bg-gray-950 overflow-y-auto">
-            <ul className="p-1">
-              {Object.keys(categoriesWithLanguages).map(name => {
-                return (
-                  <li
-                    key={name}
-                    onClick={() => {
-                      setSearchTerm('')
-                      setSelectedCategory(name)
-                    }}
-                    onKeyUp={() => {}}
-                  >
-                    <button
-                      type="button"
-                      className={clsx(
-                        'w-full cursor-pointer text-left px-3 py-2 rounded-md text-sm transition-colors',
-                        {
-                          'bg-gray-800': name === selectedCategory,
-                          'hover:bg-gray-800': name !== selectedCategory
-                        }
-                      )}
-                    >
-                      {name}
-                    </button>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="w-2/3 bg-gray-950 overflow-y-auto">
-            {!searchTerm ? (
-              <ul className="p-1">
-                {categoriesWithLanguages[selectedCategory].map(
-                  ({ langName }) => {
-                    return (
-                      <li key={langName}>
-                        <button
-                          type="button"
-                          onClick={() => {
-                            setSelectedLang(langName)
-                          }}
-                          className={clsx(
-                            'w-full inline-flex justify-between items-center cursor-pointer text-left px-3 py-2 rounded-md text-sm transition-colors',
-                            {
-                              'bg-gray-800': langName === selectedLang,
-                              'hover:bg-gray-800': langName !== selectedLang
-                            }
-                          )}
-                        >
-                          <span>{langName}</span>
-                          {langName === selectedLang && (
-                            <Check className="size-4 text-green-500 stroke-4" />
-                          )}
-                        </button>
-                      </li>
-                    )
-                  }
-                )}
-              </ul>
-            ) : (
-              <ul className="p-1">
-                {filteredResult.map(({ item: { langId, langName } }) => {
-                  return (
-                    <li key={langId}>
-                      <button
-                        type="button"
-                        onClick={() => {
-                          setSelectedLang(langName)
-                        }}
-                        className={clsx(
-                          'w-full inline-flex justify-between items-center cursor-pointer text-left px-3 py-2 rounded-md text-sm transition-colors',
-                          {
-                            'bg-gray-800': langName === selectedLang,
-                            'hover:bg-gray-800': langName !== selectedLang
-                          }
-                        )}
-                      >
-                        <span>{langName}</span>
-                        {langName === selectedLang && (
-                          <Check className="size-4 text-green-500 stroke-4" />
-                        )}
-                      </button>
-                    </li>
-                  )
-                })}
-              </ul>
-            )}
-          </div>
-        </div>
+        <LanguagePicker
+          categoriesWithLanguages={categoriesWithLanguages}
+          languages={
+            searchTerm
+              ? extractLanguages(filteredResult)
+              : (categoriesWithLanguages[selectedCategory] ?? [])
+          }
+          selectedCategory={selectedCategory}
+          selectedLanguage={selectedLanguage}
+          onSelectedCategory={handleSelectedCategory}
+          onSelectedLanguage={handleSelectedLanguage}
+        />
       </div>
       <Button type="submit" className="px-3 py-2">
         Create Snippet
