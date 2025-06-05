b536b2664bb0c718a539a19c4c1a87c6df2f1e61
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-05 01:13:42 -0500

    Add delete button to SnippetCard with onOpenModal handler

--- src/components/molecules/snippet-card.tsx
+++ src/components/molecules/snippet-card.tsx
@@ -1,4 +1,4 @@
-import { Check, Copy } from 'lucide-react'
+import { Check, Copy, Trash } from 'lucide-react'
 import { AnimatePresence, motion } from 'motion/react'
 import { useState } from 'react'
 
@@ -6,9 +6,15 @@ interface SnippetCardProps {
   title: string
   code: string
   rawCode: string
+  onOpenModal(): void
 }
 
-const SnippetCard: React.FC<SnippetCardProps> = ({ title, rawCode, code }) => {
+const SnippetCard: React.FC<SnippetCardProps> = ({
+  title,
+  rawCode,
+  code,
+  onOpenModal
+}) => {
   const [isCopied, setIsCopied] = useState(false)
 
   const handleClickCopy = () => {
@@ -22,28 +28,37 @@ const SnippetCard: React.FC<SnippetCardProps> = ({ title, rawCode, code }) => {
     <article className="border-2 border-gray-800 rounded-lg overflow-clip">
       <header className="p-2 flex items-center justify-between">
         <h2 className="text-sm font-medium">{title}</h2>
-        <button
-          type="button"
-          className="group p-2 cursor-pointer border-2 border-gray-800 rounded-sm hover:bg-gray-900 transition-colors duration-200 inline-flex items-center justify-center"
-          onClick={handleClickCopy}
-        >
-          <AnimatePresence mode="wait" initial={false}>
-            <motion.span
-              key={isCopied ? 'copied' : 'copy'}
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              exit={{ scale: 0 }}
-              transition={{ duration: 0.2 }}
-              className="inline-flex items-center justify-center"
-            >
-              {isCopied ? (
-                <Check className="opacity-70 group-hover:opacity-100 size-4" />
-              ) : (
-                <Copy className="opacity-70 group-hover:opacity-100 size-4" />
-              )}
-            </motion.span>
-          </AnimatePresence>
-        </button>
+        <div className="flex gap-2">
+          <button
+            type="button"
+            className="group p-2 cursor-pointer border-2 border-gray-800 rounded-sm hover:bg-gray-900 transition-colors duration-200 inline-flex items-center justify-center"
+            onClick={handleClickCopy}
+          >
+            <AnimatePresence mode="wait" initial={false}>
+              <motion.span
+                key={isCopied ? 'copied' : 'copy'}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+                transition={{ duration: 0.2 }}
+                className="inline-flex items-center justify-center"
+              >
+                {isCopied ? (
+                  <Check className="opacity-70 group-hover:opacity-100 size-4" />
+                ) : (
+                  <Copy className="opacity-70 group-hover:opacity-100 size-4" />
+                )}
+              </motion.span>
+            </AnimatePresence>
+          </button>
+          <button
+            type="button"
+            className="group p-2 cursor-pointer border-2 border-gray-800 rounded-sm hover:bg-gray-900 transition-colors duration-200 inline-flex items-center"
+            onClick={onOpenModal}
+          >
+            <Trash className="opacity-70 group-hover:opacity-100 size-4" />
+          </button>
+        </div>
       </header>
       <div
         className="overflow-clip"
