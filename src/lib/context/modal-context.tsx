776dd4c64a35df675fb7a45d02942fff73a6a5e7
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-06 03:46:13 -0500

    Add modal context for snippet deletion confirmation

---
+++ src/lib/context/modal-context.tsx
@@ -0,0 +1,77 @@
+import { deleteSnippet } from '@services/snippet-service'
+import { createContext, useContext, useState } from 'react'
+import { toast } from 'sonner'
+
+type SnippetToDelete = {
+  snippetId: string
+  title: string
+}
+
+interface ModalContextProps {
+  isModalOpen: boolean
+  showModal: (snippetToDelete: SnippetToDelete) => () => void
+  closeModal: () => void
+  handleConfirmDelete: () => void
+  snippetToDelete: SnippetToDelete | null
+}
+
+interface ModalProviderProps {
+  children: React.ReactNode
+}
+
+const modalContext = createContext<ModalContextProps | null>(null)
+
+export const ModalProvider = ({ children }: ModalProviderProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [snippetToDelete, setSnippetToDelete] =
+    useState<SnippetToDelete | null>(null)
+
+  const handleOpenModal = (snippetToDelete: SnippetToDelete) => () => {
+    setSnippetToDelete(snippetToDelete)
+    setIsModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleConfirmDelete = async () => {
+    if (!snippetToDelete) return
+
+    toast.promise(deleteSnippet(snippetToDelete.snippetId), {
+      loading: 'Deleting snippet...',
+      success: () => {
+        handleCloseModal()
+        return 'Snippet deleted successfully'
+      },
+      error: error => {
+        console.error(error)
+        return 'Error deleting snippet'
+      }
+    })
+  }
+
+  return (
+    <modalContext.Provider
+      value={{
+        isModalOpen,
+        showModal: handleOpenModal,
+        closeModal: handleCloseModal,
+        handleConfirmDelete,
+        snippetToDelete
+      }}
+    >
+      {children}
+    </modalContext.Provider>
+  )
+}
+
+export const useModal = () => {
+  const context = useContext(modalContext)
+
+  if (!context) {
+    throw new Error('useModal must be used within a ModalProvider')
+  }
+
+  return context
+}
