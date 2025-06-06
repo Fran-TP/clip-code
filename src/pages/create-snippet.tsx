129578d3e47fdc4951db82ebd22ab5083c09d8ac
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-06 03:45:47 -0500

    Refactor create-snippet to use form snippet context

--- src/pages/create-snippet.tsx
+++ src/pages/create-snippet.tsx
@@ -1,9 +1,8 @@
 import EditorCode from '@components/molecules/editor-code'
 import FormSnippet from '@components/molecules/form-snippet'
-import { useEditorCode } from '@lib/context/editorCodeContext'
+import { useFormSnippet } from '@lib/context/form-snippet-context'
 import fetchCategoriesWithLanguages from '@services/fetchCategoriesWithLanguages'
 import fetchLanguages from '@services/fetchLanguages'
-import { useState } from 'react'
 
 export const createSnippetLoader = async () => {
   const [categoriesWithLanguages, languages] = await Promise.all([
@@ -18,28 +17,24 @@ export const createSnippetLoader = async () => {
 }
 
 const CreateSnippet: React.FC = () => {
-  const [form, setForm] = useState({
-    title: '',
-    description: '',
-    code: ''
-  })
-  const { selectedLanguage } = useEditorCode()
+  const { formSnippet } = useFormSnippet()
+
   return (
     <>
       <h1 className="text-3xl font-bold mb-4">Create Snippet</h1>
       <div className="grid flex-1 grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="border-2 border-gray-800 rounded-md p-4 flex flex-col gap-4 bg-base">
+        <div className="border-2 border-gray-800 rounded-md px-4 py-2 flex flex-col gap-2 bg-base">
           <h2 className="text-xl font-semibold">Snippet Details</h2>
-          <FormSnippet form={form} setForm={setForm} />
+          <FormSnippet />
         </div>
         <div className="border-2 border-gray-800 overflow-clip rounded-md flex flex-col">
           <div className="p-2 flex gap-2 items-center">
             <span className="text-sm">Language</span>
             <span className="text-sm font-bold text-gray-500">
-              {selectedLanguage}
+              {formSnippet.language}
             </span>
           </div>
-          <EditorCode form={form} setForm={setForm} />
+          <EditorCode />
         </div>
       </div>
     </>
