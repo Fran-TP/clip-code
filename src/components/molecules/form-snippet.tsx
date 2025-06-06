511595294d2d0d9e54779f0d5eb6cc4cc525ad2a
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-06 03:45:22 -0500

    Refactor FormSnippet to use form-snippet context

--- src/components/molecules/form-snippet.tsx
+++ src/components/molecules/form-snippet.tsx
@@ -2,7 +2,7 @@ import Button from '@components/atoms/button'
 import Input from '@components/atoms/input'
 import Label from '@components/atoms/label'
 import LanguagePicker from '@components/molecules/language-picker'
-import { useEditorCode } from '@lib/context/editorCodeContext'
+import { useFormSnippet } from '@lib/context/form-snippet-context'
 import { extractLanguages } from '@lib/helpers/normalizeCategories'
 import type { LoaderDataCreateSnippet } from '@lib/types'
 import { createSnippet } from '@services/snippet-service'
@@ -13,28 +13,13 @@ import { useLoaderData } from 'react-router'
 import type { BundledLanguage } from 'shiki'
 import { toast } from 'sonner'
 
-interface FormSnippetProps {
-  form: {
-    title: string
-    description: string
-    code: string
-  }
-  setForm: React.Dispatch<
-    React.SetStateAction<{
-      title: string
-      description: string
-      code: string
-    }>
-  >
-}
-
-const FormSnippet: React.FC<FormSnippetProps> = ({ form, setForm }) => {
+const FormSnippet: React.FC = () => {
   const [selectedCategory, setSelectedCategory] = useState('General')
   const [searchTerm, setSearchTerm] = useState('')
 
   const { categoriesWithLanguages, languages } =
     useLoaderData<LoaderDataCreateSnippet>()
-  const { selectedLanguage, setSelectedLanguage } = useEditorCode()
+  const { formSnippet, setFormSnippet } = useFormSnippet()
 
   const fuse = new Fuse(languages, {
     keys: ['language']
@@ -47,17 +32,18 @@ const FormSnippet: React.FC<FormSnippetProps> = ({ form, setForm }) => {
     event.preventDefault()
 
     const success = await createSnippet({
-      ...form,
+      ...formSnippet,
       snippetId: crypto.randomUUID(),
-      fkLanguageId: selectedLanguage
+      fkLanguageId: formSnippet.language
     })
 
     if (success) {
-      setForm({
+      setFormSnippet(prev => ({
         title: '',
         description: '',
-        code: ''
-      })
+        code: '',
+        language: prev.language
+      }))
 
       toast.success('Snippet created successfully')
     } else {
@@ -72,7 +58,10 @@ const FormSnippet: React.FC<FormSnippetProps> = ({ form, setForm }) => {
   }
 
   const handleSelectedLanguage = (language: BundledLanguage) => () => {
-    setSelectedLanguage(language)
+    setFormSnippet(prev => ({
+      ...prev,
+      language
+    }))
   }
 
   const handleSelectedCategory = (category: string) => () => {
@@ -81,45 +70,48 @@ const FormSnippet: React.FC<FormSnippetProps> = ({ form, setForm }) => {
   }
 
   return (
-    <form className="space-y-4" onSubmit={handleSubmit}>
-      <Label htmlFor="title" className="flex flex-col gap-3">
+    <form className="space-y-3" onSubmit={handleSubmit}>
+      <Label htmlFor="title" className="flex flex-col gap-1">
         Title
         <Input
           id="title"
           type="text"
           name="title"
-          value={form.title}
+          value={formSnippet.title}
           placeholder="Title of the snippet"
           autoComplete="off"
           required
           onChange={event => {
             const value = event.currentTarget.value
 
-            setForm(prev => ({
+            setFormSnippet(prev => ({
               ...prev,
               title: value
             }))
           }}
         />
       </Label>
-      <Label htmlFor="description" className="flex flex-col gap-3">
+      <Label htmlFor="description" className="flex flex-col gap-1">
         Description
         <textarea
           id="description"
           name="description"
-          value={form.description}
+          value={formSnippet.description}
           placeholder="Description of the snippet"
           className="border-2 border-gray-800 field-sizing-content resize-none px-3 bg-gray-950 py-2 rounded-sm outline-2 outline-transparent focus-visible:outline-cyan-500 transition-colors duration-200"
           required
           onChange={event => {
             const value = event.currentTarget.value
 
-            setForm(prev => ({
+            setFormSnippet(prev => ({
               ...prev,
               description: value
             }))
           }}
         />
+      </Label>
+      <Label htmlFor="language" className="inline-block mb-1">
+        Language
       </Label>
       <div className="border-2 border-gray-800 rounded-md flex flex-col divide-gray-800 divide-y-2 bg-base">
         <div className="p-3 relative">
@@ -128,6 +120,7 @@ const FormSnippet: React.FC<FormSnippetProps> = ({ form, setForm }) => {
             name="search"
             value={searchTerm}
             placeholder="Search language"
+            autoComplete="off"
             required
             onChange={handleSearchChange}
             className="peer w-full pl-8.5 text-sm"
@@ -142,7 +135,7 @@ const FormSnippet: React.FC<FormSnippetProps> = ({ form, setForm }) => {
               : (categoriesWithLanguages[selectedCategory] ?? [])
           }
           selectedCategory={selectedCategory}
-          selectedLanguage={selectedLanguage}
+          selectedLanguage={formSnippet.language}
           onSelectedCategory={handleSelectedCategory}
           onSelectedLanguage={handleSelectedLanguage}
         />
