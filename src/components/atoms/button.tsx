fe57442367504a1f4b40f80d1c1f5b18bf62fddd
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-06 03:45:08 -0500

    Improve button focus styles for accessibility

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
     <button
       type="button"
       className={clsx(
-        'button-raised bg-cyan-500 text-gray-200 rounded-xl cursor-pointer hover:bg-cyan-600 transition-colors duration-200',
+        'button-raised bg-cyan-500 text-gray-200 rounded-xl cursor-pointer outline-2 outline-transparent outline-offset-3 focus-visible:outline-gray-700 hover:bg-cyan-600 transition-all duration-200',
         className
       )}
       {...props}
