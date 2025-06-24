d90cf956fc0e9e1ab428698980f450149f136310
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-24 13:57:32 -0500

    Refactor fetchCategoriesWithLanguages for readability

--- src/features/snippets/services/fetch-categories-with-languages.ts
+++ src/features/snippets/services/fetch-categories-with-languages.ts
@@ -1,24 +1,17 @@
-import type {
-  CategoriesWithLanguages,
-  CategoriesWithLanguagesMap
-} from '@features/snippets/types'
+import type { CategoriesWithLanguages, CategoriesWithLanguagesMap } from '@features/snippets/types'
 import { db } from '@shared/config/db-config'
 
-const fetchCategoriesWithLanguages =
-  async (): Promise<CategoriesWithLanguagesMap> => {
-    const result = await db.select<CategoriesWithLanguages[]>(`
+const fetchCategoriesWithLanguages = async (): Promise<CategoriesWithLanguagesMap> => {
+  const result = await db.select<CategoriesWithLanguages[]>(`
     SELECT c.category_id categoryId, c.name category,l.language_id languageId, l.name language
-      FROM languages_categories lc
+    FROM languages_categories lc
         JOIN main.categories c on c.category_id = lc.fk_category_id
         JOIN main.languages l on l.language_id = lc.fk_language_id;
   `)
 
-    const groupedByCategories = Object.groupBy(
-      result,
-      ({ category }) => category
-    )
+  const groupedByCategories = Object.groupBy(result, ({ category }) => category)
 
-    return groupedByCategories
-  }
+  return groupedByCategories
+}
 
 export default fetchCategoriesWithLanguages
