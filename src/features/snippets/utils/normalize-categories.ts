75a8f8b952c2dc38193041b3acebad1427a2f90d
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-30 00:23:12 -0500

    Update Languages import path in normalize-categories util

--- src/features/snippets/utils/normalize-categories.ts
+++ src/features/snippets/utils/normalize-categories.ts
@@ -1,4 +1,4 @@
-import type { Languages } from '@lib/types'
+import type { Languages } from '@features/snippets/types'
 import type { FuseResult } from 'fuse.js'
 
 export const extractLanguages = (items: FuseResult<Languages>[]) => {
