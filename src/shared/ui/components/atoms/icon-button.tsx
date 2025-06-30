85841054dfab555b4d1c6eacdcbe0216a18e19e6
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-30 00:23:54 -0500

    Refactor IconButton className for consistency and readability

--- src/shared/ui/components/atoms/icon-button.tsx
+++ src/shared/ui/components/atoms/icon-button.tsx
@@ -2,16 +2,12 @@ import clsx from 'clsx'
 
 interface IconButtonProps extends React.ComponentProps<'button'> {}
 
-const IconButton: React.FC<IconButtonProps> = ({
-  children,
-  className,
-  ...props
-}) => {
+const IconButton: React.FC<IconButtonProps> = ({ children, className, ...props }) => {
   return (
     <button
       type="button"
       className={clsx(
-        'group p-2 cursor-pointer border-2 border-gray-800 rounded-sm outline-base hover:bg-gray-900 transition-colors duration-200 inline-flex items-center',
+        'group inline-flex cursor-pointer items-center rounded-sm border-2 border-gray-800 p-2 outline-base transition-colors duration-200 hover:bg-gray-900',
         className
       )}
       {...props}
