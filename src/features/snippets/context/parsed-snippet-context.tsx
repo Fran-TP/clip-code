7cf866402202853f4c01c908ec3fb5bc38844fce
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-30 00:22:41 -0500

    Refactor ParsedSnippetProvider to support pagination and loading state

--- src/features/snippets/context/parsed-snippet-context.tsx
+++ src/features/snippets/context/parsed-snippet-context.tsx
@@ -1,65 +1,70 @@
-import type { ParsedSnippet, Snippet } from '@features/snippets/types'
-import { createContext, useContext, useEffect, useRef, useState } from 'react'
-import { useLoaderData } from 'react-router'
-import { codeToHtml } from 'shiki'
+import type { ParsedSnippet } from '@features/snippets/types'
+import { createContext, useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { fetchPaginatedSnippets } from '../services/snippet-service'
 
 interface ParsedSnippetContextProps {
   parsedSnippets: ParsedSnippet[]
   setParsedSnippets: React.Dispatch<React.SetStateAction<ParsedSnippet[]>>
+  handleNextPage: () => void
   isLoading: boolean
   isEmpty: boolean
+  hasMore: boolean
 }
 
 interface ParsedSnippetPropviderProps {
   children: React.ReactNode
 }
 
-interface LoaderData {
-  snippets: Snippet[]
-}
+const ParsedSnippetContext = createContext<ParsedSnippetContextProps | null>(null)
 
-const ParsedSnippetContext = createContext<ParsedSnippetContextProps | null>(
-  null
-)
+export const ParsedSnippetProvider = ({ children }: ParsedSnippetPropviderProps) => {
+  const [snippets, setSnippets] = useState<ParsedSnippet[]>([])
+  const [cursor, setCursor] = useState<number | null>(null)
+  const [nextCursor, setNextCursor] = useState<number | null>(null)
+  const [hasMore, setHasMore] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
-export const ParsedSnippetProvider = ({
-  children
-}: ParsedSnippetPropviderProps) => {
-  const { snippets } = useLoaderData<LoaderData>()
-  const [parsedSnippets, setParsedSnippets] = useState<ParsedSnippet[]>([])
-  const [isLoading, setIsLoading] = useState(snippets.length > 0)
-  const processedRef = useRef(false)
+  const seemCursors = useRef<Set<number | null>>(new Set())
+
+  const handleNextPage = useCallback(() => {
+    if (!hasMore || isLoading) return
+
+    setCursor(nextCursor)
+  }, [hasMore, isLoading, nextCursor])
 
   useEffect(() => {
-    if (snippets.length === 0 || processedRef.current) return
+    if (seemCursors.current.has(cursor)) return
+    seemCursors.current.add(cursor)
 
-    processedRef.current = true
+    console.log('Fetching snippets with cursor:', cursor)
+    setIsLoading(true)
+    fetchPaginatedSnippets(cursor, 15)
+      .then(async ({ snippets: newSnippets, hasMore, nextCursor }) => {
+        setSnippets(prev => [...prev, ...newSnippets])
+        setHasMore(hasMore)
+        setNextCursor(nextCursor)
+      })
+      .catch(error => {
+        console.error('Error fetching snippets:', error)
+        throw new Error(error)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [cursor])
 
-    const loadAllSnippets = async () => {
-      setIsLoading(true)
-
-      const result = await Promise.all(
-        snippets.map(async snippet => {
-          const html = await codeToHtml(snippet.code, {
-            theme: 'github-dark-default',
-            lang: snippet.fkLanguageId
-          })
-          return { ...snippet, code: html, rawCode: snippet.code }
-        })
-      )
-
-      setParsedSnippets(result)
-      setIsLoading(false)
-    }
-
-    loadAllSnippets()
-  }, [snippets])
-
-  const isEmpty = parsedSnippets.length === 0
+  const isEmpty = snippets.length === 0
 
   return (
     <ParsedSnippetContext.Provider
-      value={{ parsedSnippets, setParsedSnippets, isLoading, isEmpty }}
+      value={{
+        parsedSnippets: snippets,
+        setParsedSnippets: setSnippets,
+        handleNextPage,
+        isLoading,
+        isEmpty,
+        hasMore
+      }}
     >
       {children}
     </ParsedSnippetContext.Provider>
@@ -70,9 +75,7 @@ export const useParsedSnippets = () => {
   const context = useContext(ParsedSnippetContext)
 
   if (!context) {
-    throw new Error(
-      'useParsedSnippets must be used within a ParsedSnippetProvider'
-    )
+    throw new Error('useParsedSnippets must be used within a ParsedSnippetProvider')
   }
 
   return context
