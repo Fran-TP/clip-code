a397cb2249ac656fb3a0de61e09850ebd106b9de
Fran Torres <ftorresp@unasam.edu.pe>
2025-06-30 00:22:13 -0500

    Update snippet-card.tsx

--- src/features/snippets/components/snippet-card.tsx
+++ src/features/snippets/components/snippet-card.tsx
@@ -1,13 +1,13 @@
 import { useModal } from '@features/snippets/context/modal-context'
+import updateFavoriteSnippet from '@features/snippets/services/update-favorite-snippet'
 import AnimatedIcon from '@shared/ui/components/atoms/animated-icon'
 import IconButton from '@shared/ui/components/atoms/icon-button'
 import { Check, Copy, Star, Trash } from 'lucide-react'
 import { type FC, useState } from 'react'
 import { toast } from 'sonner'
-import updateFavoriteSnippet from '../services/update-favorite-snippet'
 
 interface SnippetCardProps {
-  snippetId: string
+  snippetId: number
   title: string
   code: string
   isFavorite: boolean
@@ -38,30 +38,30 @@ const SnippetCard: FC<SnippetCardProps> = ({ snippetId, title, rawCode, isFavori
   }
 
   return (
-    <article className="border-2 border-gray-800 rounded-lg overflow-clip">
-      <header className="p-2 flex items-center justify-between">
-        <h2 className="text-sm font-medium">{title}</h2>
+    <article className="overflow-clip rounded-lg border-2 border-gray-800">
+      <header className="flex items-center justify-between p-2">
+        <h2 className="font-medium text-sm">{title}</h2>
         <div className="flex gap-2">
           <IconButton onClick={handleClickCopy}>
             <AnimatedIcon keyAnimation={isCopied ? 'copied' : 'copy'}>
               {isCopied ? (
-                <Check className="opacity-70 group-hover:opacity-100 size-4" />
+                <Check className="size-4 opacity-70 group-hover:opacity-100" />
               ) : (
-                <Copy className="opacity-70 group-hover:opacity-100 size-4" />
+                <Copy className="size-4 opacity-70 group-hover:opacity-100" />
               )}
             </AnimatedIcon>
           </IconButton>
           <IconButton onClick={handleClickFavorite}>
             <AnimatedIcon keyAnimation={isFavoriteState ? 'favorite' : 'not-favorite'}>
               {isFavoriteState ? (
-                <Star className="opacity-70 group-hover:opacity-100 size-4 fill-gray-200" />
+                <Star className="size-4 fill-gray-200 opacity-70 group-hover:opacity-100" />
               ) : (
-                <Star className="opacity-70 group-hover:opacity-100 size-4" />
+                <Star className="size-4 opacity-70 group-hover:opacity-100" />
               )}
             </AnimatedIcon>
           </IconButton>
           <IconButton onClick={showModal({ snippetId, title })}>
-            <Trash className="opacity-70 group-hover:opacity-100 size-4" />
+            <Trash className="size-4 opacity-70 group-hover:opacity-100" />
           </IconButton>
         </div>
       </header>
