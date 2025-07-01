6d375bb80fc7ce6f2e73b78482a4b7573776cad2
Fran Torres <ftorresp@unasam.edu.pe>
2025-07-01 01:29:10 -0500

    Add page title to home screen

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,7 @@ const Home: React.FC = () => {
   return (
     <ParsedSnippetProvider>
       <ModalProvider>
+        <h1 className="mb-4 font-bold text-3xl">Snippets</h1>
         <SnippetList />
       </ModalProvider>
     </ParsedSnippetProvider>
