36f4b2b7562d7a8531640fe8a58995a1af1f0537
Fran Torres <ftorresp@unasam.edu.pe>
2025-07-01 01:29:21 -0500

    Make header sticky and add background color

--- src/shared/ui/components/molecules/header.tsx
+++ src/shared/ui/components/molecules/header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <header className="shrink-0 h-16 border-b-2 border-gray-800 px-4">
-      <div className="h-full flex items-center justify-between">
+    <header className="sticky top-0 z-10 h-16 shrink-0 border-gray-800 border-b-2 bg-gray-950 px-4">
+      <div className="flex h-full items-center justify-between">
         <div className="flex items-center gap-2 md:gap-6">
-          <h1 className="text-xl font-bold">ClipCode</h1>
+          <h1 className="font-bold text-xl">ClipCode</h1>
         </div>
       </div>
     </header>
