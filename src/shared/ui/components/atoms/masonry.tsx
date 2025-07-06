12e38f3754ae86f6af771d83905f534641c7a58a
Fran Torres <ftorresp@unasam.edu.pe>
2025-07-05 23:20:59 -0500

    Enable balanced layout in Masonry component

--- src/shared/ui/components/atoms/masonry.tsx
+++ src/shared/ui/components/atoms/masonry.tsx
@@ -8,13 +8,12 @@ interface MasonryProps<T> {
 const CONFIG = {
   columns: [1, 2],
   gap: [16, 16],
-  media: [640, 768]
+  media: [640, 768],
+  useBalancedLayout: true
 }
 
 const MasonryLayout = <T,>({ items, children }: MasonryProps<T>) => {
-  return (
-    <Masonry items={items} config={CONFIG} render={item => children(item)} />
-  )
+  return <Masonry items={items} config={CONFIG} render={item => children(item)} />
 }
 
 export default MasonryLayout
