b917872f563c2544cea3291ffcaca157f351ca37
Fran Torres <ftorresp@unasam.edu.pe>
2025-07-05 23:21:17 -0500

    Add main layout with header, sidebar, and animated routing

--- src/shared/ui/layouts/main-layout.tsx
+++ src/shared/ui/layouts/main-layout.tsx
@@ -1,1 +1,31 @@
+import Progressbar from '@shared/ui/components/atoms/progressbar'
+import Header from '@shared/ui/components/molecules/header'
+import Sidebar from '@shared/ui/components/molecules/sidenav'
+import { AnimatePresence, motion } from 'motion/react'
+import { Outlet, useLocation } from 'react-router'
+
+const MainLayout: React.FC = () => {
+  const { pathname } = useLocation()
+
+  return (
+    <div className="flex h-screen min-h-screen flex-col text-gray-200">
+      <Header />
+      <Progressbar />
       <div className="flex flex-1 overflow-x-clip">
+        <Sidebar />
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.main
+            key={pathname}
+            initial={{ x: 20 }}
+            animate={{ x: 0 }}
+            className="flex flex-1 flex-col p-4"
+          >
+            <Outlet />
+          </motion.main>
+        </AnimatePresence>
+      </div>
+    </div>
+  )
+}
+
+export default MainLayout
