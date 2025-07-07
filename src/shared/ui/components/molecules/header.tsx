7a7a1ac990277f3df1a93004b8a500671210af3a
Fran Torres <ftorresp@unasam.edu.pe>
2025-07-07 00:43:51 -0500

    Remove unused left-0 class from header container

--- src/shared/ui/components/molecules/header.tsx
+++ src/shared/ui/components/molecules/header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="sticky top-0 left-0 z-10 h-16 shrink-0 border-gray-800 border-b-2 bg-gray-950 px-4">
+    <header className="sticky top-0 z-10 h-16 shrink-0 border-gray-800 border-b-2 bg-gray-950 px-4">
       <div className="flex h-full items-center justify-between">
         <div className="flex items-center gap-2 md:gap-6">
           <h1 className="font-bold text-xl">ClipCode</h1>
