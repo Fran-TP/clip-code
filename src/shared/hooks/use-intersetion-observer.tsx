519267f2a2e822b2b434ddb59651b7822314af9c
Fran Torres <ftorresp@unasam.edu.pe>
2025-07-07 22:35:48 -0500

    Add useIntersectionObserver hook for element visibility detection

---
+++ src/shared/hooks/use-intersetion-observer.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react'
+
+interface UseIntersetionObserverProps<T> {
+  targetRef: React.RefObject<T | null>
+  options?: IntersectionObserverInit
+}
+
+const useIntersectionObserver = (
+  targetRef: React.RefObject<HTMLDivElement | null>,
+  options: IntersectionObserverInit = { threshold: 0.1 }
+) => {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+  const observer = useRef<IntersectionObserver | null>(null)
+
+  useEffect(() => {
+    if (!targetRef.current) return
+
+    observer.current = new IntersectionObserver(entries => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          setIsIntersecting(true)
+          console.log('Element is in view')
+        } else {
+          setIsIntersecting(false)
+          console.log('Element is out of view')
+        }
+      }
+    }, options)
+
+    observer.current.observe(targetRef.current)
+
+    return () => {
+      if (observer) {
+        observer.current?.disconnect()
+      }
+    }
+  }, [options, targetRef.current])
+
+  return {
+    isIntersecting,
+    observer,
+    targetRef
+  }
+}
+
+export default useIntersectionObserver
